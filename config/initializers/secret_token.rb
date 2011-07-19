6ce1db5821099e2318d57ba3c031cacee2dabd8b
Fiona McDonald <fiona.mcdonald@sage.com>
2011-07-19 11:10:10 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SjaCadets::Application.config.secret_token = 'e08654844e7865f29bdc96f1e3ade263f3d2b868bd916bf7dd967f9f10734f2d008d82727aaf6c51d4acfaebd4f495159713a7992aeeb3f379e0d0fdf13ed85e'
